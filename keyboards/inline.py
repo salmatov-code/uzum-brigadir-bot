2647c55e8b83852df7e709162548b617408c21cf
salmatov-code <s.almatov@uzum.com>
2026-07-21 20:54:05 +0500

    Update inline.py

--- keyboards/inline.py
+++ keyboards/inline.py
@@ -2,18 +2,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 def make_inline_markup(item: dict, is_admin: bool = False) -> InlineKeyboardMarkup:
-    keyboard = [
-        [
-            InlineKeyboardButton(
-                text="🎥 Видео",
-                callback_data=f"video:{item['id']}"
-            ),
-            InlineKeyboardButton(
-                text="📄 Акт",
-                callback_data=f"doc:{item['id']}"
-            )
-        ]
-    ]
+    keyboard = []
 
     if is_admin:
         keyboard.append([
