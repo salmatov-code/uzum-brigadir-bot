e3a427160126b25ce0ac36c0172b6a9130cd7048
salmatov-code <s.almatov@uzum.com>
2026-07-21 20:43:48 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,18 @@
 from aiogram import Bot, Dispatcher
 from threading import Thread
-from keep_alive import run
 import asyncio
 import logging
+
+from keep_alive import run
 from config import settings
+
 from services.sheets import SheetsService
 from services.cache import Cache
+
 from handlers.start import register_start_handlers
 from handlers.search import register_search_handlers
 from handlers.callbacks import register_callback_handlers
+from handlers.admin import register_admin_handlers
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -18,18 +22,25 @@ async def main():
     bot = Bot(token=settings.BOT_TOKEN)
     dp = Dispatcher()
 
+    # Google Sheets
     sheets = SheetsService()
+
+    # Cache
     cache = Cache(sheets)
 
+    # Запускаем обновление кэша
     asyncio.create_task(cache.start())
 
+    # Регистрация обработчиков
     register_start_handlers(dp, cache, sheets)
     register_search_handlers(dp, cache)
     register_callback_handlers(dp, sheets, cache)
+    register_admin_handlers(dp, cache, sheets)
 
+    # Keep Alive (Render)
     Thread(target=run, daemon=True).start()
 
-    logger.info("Bot started")
+    logger.info("Bot started successfully")
 
     try:
         await dp.start_polling(bot)
